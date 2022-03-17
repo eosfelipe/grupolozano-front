5deaf0740a412869f77b91fd2e47d1b1be2fe732
eosfelipe <escobedo.felipe@hotmail.com>
2022-03-17 16:34:31 -0600

    updated next.config

--- next.config.js
+++ next.config.js
@@ -3,5 +3,21 @@ module.exports = {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true
+  },
+  trailingSlash: true,
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/contact': { page: '/contact' },
+      '/market-reports': { page: '/market-reports' },
+      '/market-reports/gdt': { page: '/market-reports/gdt' },
+      '/market-reports/gdt/product': { page: '/market-reports/gdt/product' },
+      '/market-reports/graphics': { page: '/market-reports' },
+      '/market-reports/imports': { page: '/market-reports' },
+      '/milk-partners': { page: '/milk-partners' },
+      '/news': { page: '/news' },
+      '/products-services': { page: '/products-services' }
+    }
   }
 }
